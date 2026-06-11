2050935bfb6fc0e350dcd95e2de24a628a13358a
Yunis <ymammadov17@gmail.com>
2026-06-11 19:57:20 +0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+
+        System.out.println("Sum: " + sum);
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Number: " + i);
+        }
+    }
+}
